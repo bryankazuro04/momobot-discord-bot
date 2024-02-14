140369b4c90784b3865ec1a7d101bac09b8ef777
bryankazuro04 <mhdalana211201@gmail.com>
2024-02-14 21:45:29 +0700

    feat: add index.js
    add initalization config bot in index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const { token, globalPrefix } = process.env;
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { readdirSync } = require("fs");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.commands = new Collection();
+client.buttons = new Collection();
+client.commandArray = [];
+client.prefix = globalPrefix;
+
+const functionFolder = readdirSync(`./src/functions`);
+
+for (const folder of functionFolder) {
+  const functionFiles = readdirSync(`./src/functions/${folder}`).filter((file = file.endsWith(".js")));
+
+  for (const file of functionFiles) require(`./functions/${folder}/${file}`)(client);
+}
+
+client.login(token);
